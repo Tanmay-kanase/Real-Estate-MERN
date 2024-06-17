6f4dd1848ab73e713aa0841d5e9a62cabb393eb8
Tanmay-Kanase <tanmaykanase06@gmail.com>
2024-06-17 14:04:04 +0530

    created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div className='text-red-500 '>
-      Hello World 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+
+
+
+      </Routes>
+    </BrowserRouter> 
   )
 }
 
