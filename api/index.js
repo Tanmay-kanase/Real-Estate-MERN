0aec1183443c19e066ecea92dd656b9f64b892a2
Tanmay-Kanase <tanmaykanase06@gmail.com>
2024-06-17 15:22:08 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
 app.listen(8888, () => {
-    console.log('server is running on port 8888!!!!!')
-})
+  console.log("server is running on port 8888!!!!!");
+});
