8bbaebd053fd531e9b8d190c17a5a8be10debb0a
Tanmay-Kanase <tanmaykanase06@gmail.com>
2024-06-19 19:23:58 +0530

    Added redux tool

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Signin from './pages/Signin'
+import SignIn from './pages/SignIn'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
@@ -13,7 +13,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
