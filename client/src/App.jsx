07d19e01fc5cba757d07fe659b575d2bfd2a27d6
Tanmay-Kanase <tanmaykanase06@gmail.com>
2024-06-21 20:47:36 +0530

    complete image upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
+        <Route path='/about' element={<Profile />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
         </Route>
