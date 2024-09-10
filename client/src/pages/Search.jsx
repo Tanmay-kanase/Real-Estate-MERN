48266d39cb00a743546af383b8ac776d0ab14b1d
Tanmay-kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2024-09-10 20:58:10 +0530

    test

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -149,7 +149,7 @@ export default function Search() {
                     )}
                     {
                         loading && (
-                            <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                            <p className='text-xl text-slate-700 text-center w-full'>Loading....</p>
                         )
                     }
                     {
